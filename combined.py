8d2a3acd8593b228faa6cb95c31fee763d815a1d
somes1h <159097624+somes1h@users.noreply.github.com>
2024-03-05 10:01:16 +0530

    Update combined.py

--- combined.py
+++ combined.py
@@ -91,167 +91,167 @@ if option2:
 
 if option3:
     import numpy as np
-import matplotlib.pyplot as plt
-import streamlit as st
-
-process=st.radio("Select the process",["Isothermal","Adiabatic","Isobaric","Isochoric","All"])
-
-#isothermal
-
-if process=="Isothermal":
-    vol1=st.number_input('Enter the value of Volume')
-    v1=np.linspace(1,vol1,500)
-    p1=[]
-    k=st.number_input('Enter the value of pressure')
-
-    for i in range(0,500):
-        p1.append(k/v1[i])
-
-    plt.plot(v1,p1)
-    plt.title('ISOTHERMAL')
-    plt.xlabel('volume')
-    plt.ylabel('pressure')
-    plt.savefig('graph.jpg')
-    st.image('graph.jpg')
-#======================================================
-
-#adiabatic
-if process=="Adiabatic":
-    gamma=st.number_input('Enter the value of Gamma')
-    vol2 = st.number_input('Enter the value of Volume',key="1")
-    v2=np.linspace(1,vol2,500)
-    p2=[]
-    k=st.number_input('Enter the value of pressure')
-    for i in range(0,500):
-        p2.append((k/v2[i])**(gamma))
-
-    plt.plot(v2,p2)
-    plt.title('ADIABATIC')
-    plt.xlabel('volume')
-    plt.ylabel('pressure')
-    plt.savefig('graph.jpg')
-    st.image('graph.jpg')
-
-#======================================================
-
-#isobaric
-if process=="Isobaric":
-    vol3 = st.number_input('Enter the value of Volume',key="2")
-    v3=np.linspace(1,vol3,500)
-    p3=st.number_input('Enter the value of pressure')
-    P3=[]
-    for i in range(0,500):
-        P3.append(p3)
-
-    plt.plot(v3,P3)
-    plt.title('ISOBARIC')
-    plt.xlabel('volume')
-    plt.ylabel('pressure')
-    plt.savefig('graph.jpg')
-    st.image('graph.jpg')
-
-
-#========================================
-#isochoric
-if process=="Isochoric":
-    V4=[]
-    v4=st.number_input('Enter the value of Volume',key="3")
-    for i in range(0,500):
-        V4.append(v4)
-    pres4 = st.number_input('Enter the value of Pressure')
-    p4=np.linspace(1,pres4,500)
-    plt.plot(V4,p4)
-    plt.title('ISOCHORIC')
-    plt.xlabel('volume')
-    plt.ylabel('pressure')
-    plt.savefig('graph.jpg')
-    st.image('graph.jpg')
-#p=k*v   isobaric  isochoric
-
-if process=="All":
-
-    #==================================================================================
-    col1, col2 = st.columns(2)
-    with col1:
-        vol1 = st.number_input('Enter the value of Volume')
-    with col2:
-        k = st.number_input('Enter the value of pressure')
-    st.text("===========================================================================")
-    v1 = np.linspace(1, vol1, 500)
-    p1 = []
-
-    print(len(v1))
-
-    for i in range(0, 500):
-        p1.append(k * (1 / v1[i]))
-    print(len(p1))
-    plt.subplot(2, 2, 1)
-    plt.plot(v1, p1)
-    plt.title('ISOTHERMAL')
-    plt.xlabel('volume')
-    plt.ylabel('pressure')
-    #====================================================================
-    col1,col2,col3=st.columns(3)
-    with col1:
-        gamma = st.number_input('Enter the value of Gamma')
-    with col2:
-        vol2 = st.number_input('Enter the value of Volume', key="1")
-    with col3:
-        k = st.number_input('Enter the value of pressure',key="2")
-    st.text("===========================================================================")
-    v2 = np.linspace(1, vol2, 500)
-    p2 = []
-
-    for i in range(0, 500):
-        p2.append(k * (1 / v2[i]) ** (gamma))
-
-    plt.subplot(2, 2, 2)
-    plt.plot(v2, p2)
-    plt.title('ADIABATIC')
-    plt.xlabel('volume')
-    plt.ylabel('pressure')
-
-    #======================================================================
-    col1, col2 = st.columns(2)
-    with col1:
-        vol3 = st.number_input('Enter the value of Volume', key="3")
-    with col2:
-        p3 = st.number_input('Enter the value of pressure',key="4")
-    v3 = np.linspace(1, vol3, 500)
-    st.text("===========================================================================")
-    P3 = []
-    for i in range(0, 500):
-        P3.append(p3)
-
-    plt.subplot(2, 2, 3)
-    plt.plot(v3, P3)
-    plt.title('ISOBARIC')
-    plt.xlabel('volume')
-    plt.ylabel('pressure')
-
-    #==========================================================================
-
-    V4 = []
-    col1, col2 = st.columns(2)
-    with col1:
-        v4 = st.number_input('Enter the value of Volume', key="5")
-    with col2:
+    import matplotlib.pyplot as plt
+    import streamlit as st
+    
+    process=st.radio("Select the process",["Isothermal","Adiabatic","Isobaric","Isochoric","All"])
+    
+    #isothermal
+    
+    if process=="Isothermal":
+        vol1=st.number_input('Enter the value of Volume')
+        v1=np.linspace(1,vol1,500)
+        p1=[]
+        k=st.number_input('Enter the value of pressure')
+    
+        for i in range(0,500):
+            p1.append(k/v1[i])
+    
+        plt.plot(v1,p1)
+        plt.title('ISOTHERMAL')
+        plt.xlabel('volume')
+        plt.ylabel('pressure')
+        plt.savefig('graph.jpg')
+        st.image('graph.jpg')
+    #======================================================
+    
+    #adiabatic
+    if process=="Adiabatic":
+        gamma=st.number_input('Enter the value of Gamma')
+        vol2 = st.number_input('Enter the value of Volume',key="1")
+        v2=np.linspace(1,vol2,500)
+        p2=[]
+        k=st.number_input('Enter the value of pressure')
+        for i in range(0,500):
+            p2.append((k/v2[i])**(gamma))
+    
+        plt.plot(v2,p2)
+        plt.title('ADIABATIC')
+        plt.xlabel('volume')
+        plt.ylabel('pressure')
+        plt.savefig('graph.jpg')
+        st.image('graph.jpg')
+    
+    #======================================================
+    
+    #isobaric
+    if process=="Isobaric":
+        vol3 = st.number_input('Enter the value of Volume',key="2")
+        v3=np.linspace(1,vol3,500)
+        p3=st.number_input('Enter the value of pressure')
+        P3=[]
+        for i in range(0,500):
+            P3.append(p3)
+    
+        plt.plot(v3,P3)
+        plt.title('ISOBARIC')
+        plt.xlabel('volume')
+        plt.ylabel('pressure')
+        plt.savefig('graph.jpg')
+        st.image('graph.jpg')
+    
+    
+    #========================================
+    #isochoric
+    if process=="Isochoric":
+        V4=[]
+        v4=st.number_input('Enter the value of Volume',key="3")
+        for i in range(0,500):
+            V4.append(v4)
         pres4 = st.number_input('Enter the value of Pressure')
-    for i in range(0, 500):
-        V4.append(v4)
-    st.text("===========================================================================")
-    p4 = np.linspace(1, pres4, 500)
-    plt.subplot(2, 2, 4)
-    plt.plot(V4, p4)
-    plt.title('ISOCHORIC')
-    plt.xlabel('volume')
-    plt.ylabel('pressure')
-
-    plt.subplot(2, 2, 4)
-    plt.plot(V4, p4)
-    plt.tight_layout()
-    plt.savefig('graph.jpg')
-    st.image('graph.jpg')
+        p4=np.linspace(1,pres4,500)
+        plt.plot(V4,p4)
+        plt.title('ISOCHORIC')
+        plt.xlabel('volume')
+        plt.ylabel('pressure')
+        plt.savefig('graph.jpg')
+        st.image('graph.jpg')
+    #p=k*v   isobaric  isochoric
+    
+    if process=="All":
+    
+        #==================================================================================
+        col1, col2 = st.columns(2)
+        with col1:
+            vol1 = st.number_input('Enter the value of Volume')
+        with col2:
+            k = st.number_input('Enter the value of pressure')
+        st.text("===========================================================================")
+        v1 = np.linspace(1, vol1, 500)
+        p1 = []
+    
+        print(len(v1))
+    
+        for i in range(0, 500):
+            p1.append(k * (1 / v1[i]))
+        print(len(p1))
+        plt.subplot(2, 2, 1)
+        plt.plot(v1, p1)
+        plt.title('ISOTHERMAL')
+        plt.xlabel('volume')
+        plt.ylabel('pressure')
+        #====================================================================
+        col1,col2,col3=st.columns(3)
+        with col1:
+            gamma = st.number_input('Enter the value of Gamma')
+        with col2:
+            vol2 = st.number_input('Enter the value of Volume', key="1")
+        with col3:
+            k = st.number_input('Enter the value of pressure',key="2")
+        st.text("===========================================================================")
+        v2 = np.linspace(1, vol2, 500)
+        p2 = []
+    
+        for i in range(0, 500):
+            p2.append(k * (1 / v2[i]) ** (gamma))
+    
+        plt.subplot(2, 2, 2)
+        plt.plot(v2, p2)
+        plt.title('ADIABATIC')
+        plt.xlabel('volume')
+        plt.ylabel('pressure')
+    
+        #======================================================================
+        col1, col2 = st.columns(2)
+        with col1:
+            vol3 = st.number_input('Enter the value of Volume', key="3")
+        with col2:
+            p3 = st.number_input('Enter the value of pressure',key="4")
+        v3 = np.linspace(1, vol3, 500)
+        st.text("===========================================================================")
+        P3 = []
+        for i in range(0, 500):
+            P3.append(p3)
+    
+        plt.subplot(2, 2, 3)
+        plt.plot(v3, P3)
+        plt.title('ISOBARIC')
+        plt.xlabel('volume')
+        plt.ylabel('pressure')
+    
+        #==========================================================================
+    
+        V4 = []
+        col1, col2 = st.columns(2)
+        with col1:
+            v4 = st.number_input('Enter the value of Volume', key="5")
+        with col2:
+            pres4 = st.number_input('Enter the value of Pressure')
+        for i in range(0, 500):
+            V4.append(v4)
+        st.text("===========================================================================")
+        p4 = np.linspace(1, pres4, 500)
+        plt.subplot(2, 2, 4)
+        plt.plot(V4, p4)
+        plt.title('ISOCHORIC')
+        plt.xlabel('volume')
+        plt.ylabel('pressure')
+    
+        plt.subplot(2, 2, 4)
+        plt.plot(V4, p4)
+        plt.tight_layout()
+        plt.savefig('graph.jpg')
+        st.image('graph.jpg')
 if option4:
     def efficiency_of_heat_engine(source_temp, sink_temp):
         efficiency = 1 - (sink_temp / source_temp)
